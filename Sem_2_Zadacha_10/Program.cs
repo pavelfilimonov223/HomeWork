c23eb93c19d77dd7c592566400c5583903ebc895
Pavel_Filimonov <filimonov223@yandex.ru>
2023-10-10 00:11:27 +0500

    Решил задачу № 10

---
+++ Sem_2_Zadacha_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача. На вход трехзначное число, на вывод показать вторую цифру числа.
+
+System.Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a>99 && a<1000)
+{
+    System.Console.WriteLine($"{a}->{(a%100-a%10)/10}");
+} else
+{
+    System.Console.Write("Это число не является трехзначным !");
+}
