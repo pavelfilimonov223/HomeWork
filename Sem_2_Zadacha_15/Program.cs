32b3b5b33de477757dac6977552da7ac6dd31264
Pavel_Filimonov <filimonov223@yandex.ru>
2023-10-09 23:08:16 +0500

    Сохранил решение задачи № 15

--- Sem_2_Zadacha_15/Program.cs
+++ Sem_2_Zadacha_15/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача. Принимает на вход цифру, день недели, проверяет выходной ли этот день.
+
+System.Console.Write("Введите номер дня недели: ");
+int dnum = Convert.ToInt32(Console.ReadLine());
+if (dnum == 1)
+{
+    Console.WriteLine($"{dnum} -> НЕТ");
+} else if (dnum ==2)
+{
+    Console.WriteLine($"{dnum} -> НЕТ");
+} else if (dnum ==3)
+{
+   Console.WriteLine($"{dnum} -> НЕТ"); 
+} else if (dnum ==4)
+{
+    Console.WriteLine($"{dnum} -> НЕТ");
+} else if (dnum ==5)
+{
+    Console.WriteLine($"{dnum} -> НЕТ");
+} else if (dnum ==6)
+{
+    Console.WriteLine($"{dnum} -> ДА, это выходной");
+} else if (dnum ==7)
+{
+    Console.WriteLine($"{dnum} -> ДА, это выходной");
+} else
+{
+    Console.WriteLine($"Ошибка ввода, надо от 1 до 7 дней в неделе");
+}
