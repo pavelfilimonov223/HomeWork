2b63dfdfb41fc781dccf12af5bfc5cc5df26cf75
Pavel_Filimonov <filimonov223@yandex.ru>
2023-10-10 16:23:21 +0500

    исправил синтаксическую ошибку в решении 15, теперь работает)

--- Sem_2_Zadacha_15/Program.cs
+++ Sem_2_Zadacha_15/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Задача. Принимает на вход цифру - день недели, проверяет выходной ли этот день.
 System.Console.Write("Введите номер дня недели: ");
 int dnum = Convert.ToInt32(Console.ReadLine());
-if(dnum => 1 && dnum <= 5)
+if(dnum >= 1 && dnum <= 5)
 {
     Console.WriteLine($"{dnum} -> НЕТ, иди работай !");
-} else if(dnum => 6 && dnum <= 7)
+} else if(dnum >= 6 && dnum <= 7)
 {
    Console.WriteLine($"{dnum} -> ДА, это выходной"); 
 } else 
